457ad81db85e79c47519f2f0ce2bff87cf803594
Braiskiskos <bmartinezparedes@danielcastelao.org>
2021-12-04 22:04:46 +0100

    Funciona Bien

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -1,11 +1,96 @@
 package com.example.firebase
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Inicializar FireBase Auth
+        auth = Firebase.auth
+
+        val darseDeAlta: Button = findViewById(R.id.botonLoguearse)
+        darseDeAlta.setOnClickListener{
+            val usuario: EditText = findViewById(R.id.correoUsuario)
+            val contraseña: EditText = findViewById(R.id.contraseñaUsuario)
+            crearCuenta(usuario.text.toString(),contraseña.text.toString())
+        }
+        val accederCuenta: Button = findViewById(R.id.botonIniciarsesion)
+        accederCuenta.setOnClickListener{
+            val usuario: EditText = findViewById(R.id.correoUsuario)
+            val contraseña: EditText = findViewById(R.id.contraseñaUsuario)
+            accederCuenta(usuario.text.toString(),contraseña.text.toString())
+        }
     }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun crearCuenta(email: String, password: String) {
+        //Crear nuevo usuario
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.i("crear b", "Crear Usuario Bien")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.i("crear m", "Crear Usuario Mal", task.exception)
+                    Toast.makeText(baseContext, "Fallo en autenticación.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [Fin Crear Cuenta]
+    }
+    private fun accederCuenta (email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("bien", "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                    Toast.makeText(baseContext, "Bien.",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("mal", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    private fun reload() {
+
+    }
+
+
 }
